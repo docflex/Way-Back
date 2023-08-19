2195e5b3b420d32aca4df7b8259a39ab2eaa9c4e
Rehber Moin <rehbermoin.io@gmail.com>
2023-08-19 09:43:45 +0530

    [FIXED]: The Persistent Fetch Script doesn't break upon Enpoint Breakage

--- backend/persistFetch.py
+++ backend/persistFetch.py
@@ -1,10 +1,21 @@
 import time
 import requests
+import sys
 
 i = 1
 while i < 10:
     url = 'http://127.0.0.1:8000/videos/'  # Change the URL to your actual API endpoint
-    response = requests.get(url)
-    print('Fetched YouTube videos:', response.status_code)
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print('Fetched YouTube videos:', response.status_code)
+        else:
+            print('Unexpected response status code:', response.status_code)
+            sys.exit(1)  # Exit with non-zero status code
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        sys.exit(1)  # Exit with non-zero status code
+    
     time.sleep(10)  # Wait for 10 seconds
     i += 1
