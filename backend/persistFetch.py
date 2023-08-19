32aa23fe331b5f2f6de1e31583623229fc8f6cca
Rehber Moin <rehbermoin.io@gmail.com>
2023-08-19 09:46:20 +0530

    [UPDATE]: Persistent Fetch has some Comfort Features Built into it. It works better and has added benefit of adding only those Videos that haven't been seen yet.

--- backend/persistFetch.py
+++ backend/persistFetch.py
@@ -2,14 +2,30 @@ import time
 import requests
 import sys
 
-i = 1
-while i < 10:
+# Initialize a set to keep track of fetched video IDs
+fetched_video_ids = set()
+
+def print_response(response):
+    print("Here are the Newly Fetched Videos:\n")
+    data = response.json()["data"]
+    items = data["items"]
+    for item in items:
+        video_id = item["video_id"]
+        if video_id not in fetched_video_ids:
+            fetched_video_ids.add(video_id)
+            print("Video ID:", video_id)
+            print("Title:", item["title"])
+            print("Published Time:", item["published_at"])
+            print("\n")
+
+while True:
     url = 'http://127.0.0.1:8000/videos/'  # Change the URL to your actual API endpoint
     
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            print('Fetched YouTube videos:', response.status_code)
+            print('Successfully Fetched YouTube videos:', response.status_code)
+            print_response(response)  # Print new video data
         else:
             print('Unexpected response status code:', response.status_code)
             sys.exit(1)  # Exit with non-zero status code
@@ -18,4 +34,3 @@ while i < 10:
         sys.exit(1)  # Exit with non-zero status code
     
     time.sleep(10)  # Wait for 10 seconds
-    i += 1
